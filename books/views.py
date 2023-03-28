d4a9bed7498568fd5040619fc1a5ae22fdb72a4c
Igor Ter-Pogosyan <igor-526@yandex.ru@git config --global user.email igor-526@yandex.rugit config --global user.email igor-526@yandex.ru@>
2023-03-28 21:12:43 +0300

    First task

---
+++ books/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse
+from books.models import Book
+
+
+def books_view(request):
+    template = 'books/books_list.html'
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, template, context)
